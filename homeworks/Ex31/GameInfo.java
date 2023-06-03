b578db98f19650f51f303371f8f37548b31bb2e4
WasiimSheb <123734906+WasiimSheb@users.noreply.github.com>
2023-06-03 19:55:15 +0300

    Update GameInfo.java

--- homeworks/Ex31/GameInfo.java
+++ homeworks/Ex31/GameInfo.java
@@ -4,7 +4,7 @@ import exe.ex3.game.PacManAlgo;
  * Make sure you update your details below!
  */
 public class GameInfo {
-	public static final String MY_ID = "not your business";
+	public static final String MY_ID = "not your business ;)";
 	public static final int CASE_SCENARIO = 2; // [0,4]
 	public static final long RANDOM_SEED = 31; // Random seed
 	public static final boolean CYCLIC_MODE = true;
